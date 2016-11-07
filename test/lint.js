6db8c6fd452101c41563f91d2fbc5bbbdaafb8ab
osaton <o@sploosh.org>
2016-11-07 08:32:04 +0200

    Add rules to check test/main.js

--- test/lint.js
+++ test/lint.js
@@ -20,8 +20,19 @@ describe('Lint', function() {
     done();
   });
 
-  it('test/main.js should pass lint rules', function(done) {
+  it('test/lint.js should pass lint rules', function(done) {
     report = cli.executeOnFiles(['test/lint.js']);
+    if (report.errorCount > 0 || report.warningCount > 0) {
+      console.log(formatter(report.results));
+    }
+
+    should(report.errorCount).equal(0);
+    should(report.warningCount).equal(0);
+    done();
+  });
+
+  it('test/main.js should pass lint rules', function(done) {
+    report = cli.executeOnFiles(['test/main.js']);
     if (report.errorCount > 0 || report.warningCount > 0) {
       console.log(formatter(report.results));
     }
